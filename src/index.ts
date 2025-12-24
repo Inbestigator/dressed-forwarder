e2211240610af5a03a934fe78d05510d56a07306
Inbestigator <inbestigatoryt@gmail.com>
2025-12-24 01:41:44 -0800

    feat: event types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { type ConnectionActions, createConnection } from "@dressed/ws";
-import { ApplicationWebhookType, GatewayDispatchEvents } from "discord-api-types/v10";
+import { ApplicationWebhookType, GatewayDispatchEvents, type GatewayDispatchPayload } from "discord-api-types/v10";
 import { botEnv } from "dressed/utils";
 
 type HandlerActions = Omit<ConnectionActions, "emit" | "shards">;
+
+export type Event<K extends keyof typeof GatewayDispatchEvents> = (GatewayDispatchPayload extends infer U
+  ? U extends { t: infer T }
+    ? (typeof GatewayDispatchEvents)[K] extends T
+      ? U
+      : never
+    : never
+  : never)["d"];
 
 /**
  * Create a forwarding connection to send event types to your webserver
